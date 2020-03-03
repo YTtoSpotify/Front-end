72dfc44bcb26030ef4be7a67a5257c9c72e76616
Jeff Lapp <lappjeff20@gmail.com>
2020-03-03 16:22:23 -0500

    Working on login button functionality

--- fe/src/app/auth/auth/auth.component.ts
+++ fe/src/app/auth/auth/auth.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.scss"]
 })
 export class AuthComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  onLogin() {
+    this.authService.login();
   }
 
+  ngOnInit() {}
 }
