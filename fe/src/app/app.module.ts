52a2f88d98d78c8c4318aff7f1692bc590c7aa2a
Jeff Lapp <lappjeff20@gmail.com>
2020-03-03 16:40:04 -0500

    Set up popup window for logging in

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth/auth.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AuthComponent } from "./auth/auth/auth.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AuthComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- fe/src/app/auth/auth.service.ts
+++ fe/src/app/auth/auth.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  login() {}
+  login() {
+    console.log("login triggered");
+    const url = "http://localhost:5000/api/auth";
+    const name = "spotify_login";
+    const specs = "width=500,height=500";
+    window.open(url, name, specs);
+
+    // this.http.get("http://localhost:5000/api/auth").subscribe(data => {
+    //   console.log(data);
+    // });
+  }
 }
