5c8a6facdc0d8147f91e160f944431478eb52416
Jeff Lapp <lappjeff20@gmail.com>
2020-03-28 14:05:34 -0400

    Built out basic login button functionality

--- fe/src/app/reusable/auth/login-button/login-button.component.ts
+++ fe/src/app/reusable/auth/login-button/login-button.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from "./../../../services/auth.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-login-button',
-  templateUrl: './login-button.component.html',
-  styleUrls: ['./login-button.component.scss']
+  selector: "app-login-button",
+  templateUrl: "./login-button.component.html",
+  styleUrls: ["./login-button.component.scss"]
 })
 export class LoginButtonComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLogin() {
+    this.authService.login();
   }
-
 }
