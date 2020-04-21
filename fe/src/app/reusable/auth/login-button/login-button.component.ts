5c8a837580b2e4780d2c1739db3e765a6c626c3a
Jeff Lapp <lappjeff20@gmail.com>
2020-04-21 14:33:54 -0400

    fixed linter error caused by private authservie

--- fe/src/app/reusable/auth/login-button/login-button.component.ts
+++ fe/src/app/reusable/auth/login-button/login-button.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./login-button.component.scss"],
 })
 export class LoginButtonComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(public authService: AuthService) {}
 
   ngOnInit() {}
 
