656019bccf92e67ec730fc57d37bb38adb53dfa5
Jeff Lapp <lappjeff20@gmail.com>
2020-04-22 19:54:34 -0400

    created subscription for search input

--- fe/src/app/channels/channels.component.ts
+++ fe/src/app/channels/channels.component.ts
@@ -1,16 +1,52 @@
 import { ChannelsService } from "../services/channels.service";
-import { Component, OnInit } from "@angular/core";
-import { Channel } from "../interfaces/channel.interface";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnDestroy,
+} from "@angular/core";
+import { fromEvent, Subscription } from "rxjs";
+import { map, debounceTime, distinctUntilChanged } from "rxjs/operators";
 
 @Component({
   selector: "app-channels",
   templateUrl: "./channels.component.html",
   styleUrls: ["./channels.component.scss"],
 })
-export class ChannelsComponent implements OnInit {
+export class ChannelsComponent implements OnInit, OnDestroy {
+  private inputEventSub: Subscription;
+  public nameFilter: string;
+
+  @ViewChild("channelSearchInput", { static: true })
+  channelSearchInput: ElementRef;
   constructor(public channelsService: ChannelsService) {}
 
   ngOnInit() {
     this.channelsService.getChannels();
+
+    this.nameFilter = this.channelsService.nameFilter;
+
+    this.inputEventSub = fromEvent(
+      this.channelSearchInput.nativeElement,
+      "keyup"
+    )
+      .pipe(
+        //  get value of input
+        map((event: any) => {
+          return event.target.value;
+        }),
+        // wait x milliseconds between key event to make request
+        debounceTime(1500),
+        // if current query is different that previous query
+        distinctUntilChanged()
+      )
+      .subscribe(() => {
+        this.channelsService.getChannels(this.channelsService.page);
+      });
+  }
+
+  ngOnDestroy() {
+    this.inputEventSub.unsubscribe();
   }
 }
