52687c6d451029174656dfe2f1552e17f8548e5a
Jeff Lapp <lappjeff20@gmail.com>
2020-04-22 13:39:15 -0400

    created channels component logic/layout

--- fe/src/app/channels/channels.component.ts
+++ fe/src/app/channels/channels.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { ChannelsService } from "./../channels.service";
+import { Component, OnInit } from "@angular/core";
+import { Channel } from "../interfaces/channel.interface";
 
 @Component({
-  selector: 'app-channels',
-  templateUrl: './channels.component.html',
-  styleUrls: ['./channels.component.scss']
+  selector: "app-channels",
+  templateUrl: "./channels.component.html",
+  styleUrls: ["./channels.component.scss"],
 })
 export class ChannelsComponent implements OnInit {
-
-  constructor() { }
+  public channels: Channel[];
+  constructor(public channelsService: ChannelsService) {}
 
   ngOnInit() {
+    this.channels = this.channelsService.channels;
   }
-
 }
