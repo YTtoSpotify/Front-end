1cd23d16627c1883c3c1d5e23a8e72145323d416
Jeff Lapp <lappjeff20@gmail.com>
2020-04-23 16:38:43 -0400

    created user interface

---
+++ fe/src/app/interfaces/user.interface.ts
@@ -0,0 +1,12 @@
+export interface User {
+  hasPlaylist: boolean;
+  _id: string;
+  email: string;
+  subbedChannels: [];
+  displayName: string;
+  username: string;
+  spotifyId: string;
+  photo: string;
+  firstName: string;
+  lastName: string;
+}
