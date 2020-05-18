aa6b45db518068f3642ca3a2d02f673c0fc93cbd
Jeff Lapp <lappjeff20@gmail.com>
2020-05-18 16:15:19 -0400

    started work on adding channel functionality

--- fe/src/app/add-channel/add-channel.component.ts
+++ fe/src/app/add-channel/add-channel.component.ts
@@ -1,4 +1,7 @@
+import { NotyfFlashService } from "./../services/notyf.service";
+import { ChannelsService } from "./../services/channels.service";
 import { Component, OnInit } from "@angular/core";
+import { isValidYTUrl } from "../reusable/youtubeUrlValidator";
 
 @Component({
   selector: "app-add-channel",
@@ -7,7 +10,24 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AddChannelComponent implements OnInit {
   public channelUrl: string;
-  constructor() {}
+  constructor(
+    private channelsService: ChannelsService,
+    private notyfService: NotyfFlashService
+  ) {}
 
   ngOnInit() {}
+
+  handleAddChannel() {
+    if (isValidYTUrl(this.channelUrl)) {
+      this.channelsService.createChannel(this.channelUrl).subscribe(
+        (data) => {
+          this.channelsService.setChannelPaginationData(data);
+          this.notyfService.successNotyf("Channel added successfully!");
+        },
+        (err) => {
+          this.notyfService.errorNotyf(err);
+        }
+      );
+    }
+  }
 }
