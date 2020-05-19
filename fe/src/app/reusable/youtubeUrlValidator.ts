f38b1c378b781346074cbca9e0c433c2ecb43f62
Jeff Lapp <lappjeff20@gmail.com>
2020-05-19 15:44:13 -0400

    finalized UI for submitting a new channel

--- fe/src/app/reusable/youtubeUrlValidator.ts
+++
@@ -1,8 +0,0 @@
-export function isValidYTUrl(url: string): boolean {
-  const urlRe = new RegExp(
-    "((http|https)://|)(www.|)youtube.com/(channel/|user/)[a-zA-Z0-9-]{1,}",
-    "gi"
-  );
-  const isValid = urlRe.test(url);
-  return isValid ? true : false;
-}
